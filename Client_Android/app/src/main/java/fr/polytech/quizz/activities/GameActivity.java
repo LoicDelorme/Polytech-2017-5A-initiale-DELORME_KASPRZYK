056102d5321ff9c1d717c7fd19d512b76f4a0b96
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-10-22 11:28:38 +0200

    Add 2 new layouts (home and game) + add internationalization support (FR and EN)

---
+++ Client_Android/app/src/main/java/fr/polytech/quizz/activities/GameActivity.java
@@ -0,0 +1,10 @@
+package fr.polytech.quizz.activities;
+
+import fr.polytech.quizz.R;
+
+public class GameActivity extends AbstractActivity {
+
+    public GameActivity() {
+        super(R.layout.activity_game);
+    }
+}
