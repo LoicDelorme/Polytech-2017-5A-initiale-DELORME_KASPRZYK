fd346120bbedde27b937d9a575d6dbdac9d36bbb
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-10-24 19:23:56 +0200

    Add beer application

--- Client_Android/app/src/main/java/fr/polytech/quizz/activities/IHome.java
+++ Client_Android/app/src/main/java/fr/polytech/quizz/activities/IHome.java
@@ -1,10 +1,10 @@
 package fr.polytech.quizz.activities;
 
+import fr.polytech.quizz.entities.Beer;
+
 public interface IHome {
 
-    public void notifyModeHasBeenSelected(Mode mode);
+    public void notifyRetrieveBeers();
 
-    public void notifyAnswerHasBeenSelected(CharSequence answer);
-
-    public void notifyNoMoreQuestions();
+    public void notifyBeerHasBeenSelected(Beer beer);
 }
